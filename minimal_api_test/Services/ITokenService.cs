920efb2151880e4fea3b07a06427c57c995e08c9
Victor Vasconcelos de Aquino <victor.vasconcelos181@gmail.com>
2023-09-01 15:20:16 -0300

    Including JWT authentication for tests

---
+++ minimal_api_test/Services/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using minimal_api_test.Entities;
+
+namespace minimal_api_test.Services;
+
+public interface ITokenService
+{
+    string GenerateToken(string key, string issuer, string audience, UserModel user);
+}
